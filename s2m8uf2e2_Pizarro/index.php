61d8d87ccb8a05a5f822661895a7357da7449d90
ipizarro2019crn <56316849+ipizarro2019crn@users.noreply.github.com>
2020-04-16 17:45:24 +0200

    Add files via upload

---
+++ s2m8uf2e2_Pizarro/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/960_12_col.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body class="container_12">
+  <div class="cap">
+      <div class="titulo grid_12">
+        <h1>carsandfilms</h1>
+      </div>
+
+      <div class="portada grid_12">
+      </div>
+
+      <div class="fondo grid_12">
+
+      </div>
+
+  </div>
+
+  <div class="cos">
+    <div class="small pulp grid_4 ">
+      <h4>Pulp Fiction</h4>
+    </div>
+
+
+    <div class="small taxi grid_4 ">
+      <h4>Taxi Driver</h4>
+    </div>
+
+
+    <div class="small gold grid_4 ">
+      <h4>Goldfinger</h4>
+    </div>
+
+    <div class="small busters grid_4 ">
+      <h4>Ghost Busters</h4>
+    </div>
+
+
+    <div class="small bean grid_4">
+      <h4>Mr.Bean</h4>
+    </div>
+
+
+    <div class="small future grid_4">
+      <h4>Back to the future</h4>
+    </div>
+
+    <div class="small rider grid_4">
+      <h4>Knight Rider</h4>
+    </div>
+
+
+    <div class="small team grid_4">
+      <h4>The A-Team</h4>
+    </div>
+
+
+    <div class="small bad grid_4">
+      <h4>Breaking Bad</h4>
+    </div>
+
+  </div>
+
+
+  <div class="peu grid_4 push_4">
+
+  </div>
+
+
+
+
+
+
+</body>
+</html>
